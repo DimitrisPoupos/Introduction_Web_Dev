60b81e9537cd83438ec77770deaa39b5c6f0fc9c
DimitrisP <108548087+DimitrisPoupos@users.noreply.github.com>
2024-11-29 18:44:47 +0200

    Rock Paper Scissors Finished
    Rock Paper Scissors game is finished.

--- javascript.js
+++ javascript.js
@@ -2,62 +2,68 @@
 // 2 = paper
 // 3 = scissors
 
-counter = computerScore = humanScore = 0;
+
 
 function getComputerChoice() {
-    //return getRandomInt(3) + 1;
-    return 1;
+    let result = Math.floor(Math.random() * 3 + 1) ;
+    switch( result ){
+        case 1 :
+            console.log("Computer played Rock");
+            break;
+        case 2 :
+            console.log("Computer played Paper");
+            break;
+        case 3 :
+            console.log("Computer played Scissors");
+            break;
+        default:
+            console.log("Computer Error"); 
+    }
+    return result;
 }
 
 function getHumanChoice() {
     let human_result = prompt("Please enter your choice", "Paper");
 
     switch ( human_result.toLowerCase() ){
-        case ( "rock") :
+        case ("rock") :
             return 1;
-        case ( "paper") :
+        case ("paper") :
             return 2;
-        case ( "scissors"):
+        case ("scissors"):
             return 3;
         default:
             return 4;
     }
 }
 
-function playRound( human_choice , computer_choise ) {
-    counter++;
-    switch ( human_choice ){
-        case 1 :
-            humanScore += (computer_choise === 3) ? 1 : 0;
-            computerScore += (computer_choise === 2) ? 1 : 0;
-            break;
-        case 2 :
-            humanScore += (computer_choise === 1) ? 1 : 0;
-            computerScore += (computer_choise === 3) ? 1 : 0;
-            break;
-        case 3 :
-            humanScore += (computer_choise === 2) ? 1 : 0;
-            computerScore += (computer_choise === 1) ? 1 : 0;
-            break;
-        case 4 :
-            alert("Wrong input, try again.");
-            counter--;
-            break;
-        default:
+function playGame( ){
+    counter = computerScore = humanScore = 0;
 
+    function playRound( human_choice , computer_choise ) {
+            counter++;
+            if (human_choice == computer_choise){
+                console.log("The round is a draw")
+            }else if ( (human_choice === 1 && computer_choise === 2) || (human_choice === 2 && computer_choise === 3) || (human_choice === 3 && computer_choise === 1) ) {
+                computerScore++;
+                console.log("The round is won by the computer " + computerScore);
+            }else{
+                humanScore++;
+                console.log("The round is won by you " + humanScore);
+            }
+        }
+
+    for(let i= 0; i<5; i++){
+        playRound( getHumanChoice(), getComputerChoice() );
     }
 
-    if ( counter === 1 ){
-        if (humanScore > computerScore){
-            console.log("Human wins");
-        }else if (humanScore < computerScore) {
-            console.log("Computer wins");
-        }else{
-            console.log("It's a draw");
-        }
+    if (humanScore > computerScore){
+        console.log("Human wins");
+    }else if (humanScore < computerScore) {
+        console.log("Computer wins");
+    }else{
+        console.log("It's a draw");
     }
 
 }
-// getComputerChoice();
-// getHumanChoice();
-playRound( getHumanChoice(), getComputerChoice() );
+playGame( );
